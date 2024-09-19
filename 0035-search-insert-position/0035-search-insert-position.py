5b66eca6db6a80243c735cf97a1bdeae059124de
Aravsharma1 <73394747+Aravsharma1@users.noreply.github.com>
2024-09-19 01:01:03 -0400

    Time: 55 ms (18.67%), Space: 17.3 MB (36.61%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -2,7 +2,7 @@ class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         low = 0
         high = len(nums) - 1
-        # simple binary search implementation
+        # simple binary search implementation: O(log n) solution
         while(low <= high):
             mid = (low + high) // 2 #integer divison, not float divison 
             if nums[mid] == target:
