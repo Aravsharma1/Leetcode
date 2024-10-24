4e13d74abb04220c13965ab2ec80382a11f09381
Aravsharma1 <73394747+Aravsharma1@users.noreply.github.com>
2024-10-24 01:37:53 -0400

    Time: 2 ms (96.47%), Space: 17.9 MB (9.61%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,12 +1,22 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        arr = [0,0]
-        length = len(nums)
-        for x in range(0,length):
-            for y in range(x+1, length):
-                if (nums[x] + nums[y]) == target:
-                    arr[0] = x
-                    arr[1] = y
-                    return arr
-                
+        # # inefficient solution:
+        # arr = [0,0]
+        # length = len(nums)
+        # for x in range(0,length):
+        #     for y in range(x+1, length):
+        #         if (nums[x] + nums[y]) == target:
+        #             arr[0] = x
+        #             arr[1] = y
+        #             return arr
+        # efficient solution: 
+        # create a default dictionary 
+        dictionary = {}
+        for x in range(len(nums)):
+            dictionary[nums[x]] = x
         
+        for x in range(len(nums)):
+            y = nums[x]
+            element = target - y
+            if element in dictionary and dictionary[element] != x:
+                return x, dictionary[element]
