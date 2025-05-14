3fdd35bc71ab9a18b5fa7da3ae53290a90fd850e
Aravsharma1 <73394747+Aravsharma1@users.noreply.github.com>
2025-05-13 21:27:34 -0400

    Time: 19 ms (15.99%), Space: 17.8 MB (86.01%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,26 +1,29 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        if len(s) != len(t):
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if(len(s) != len(t)):
             return False
-        # building the hashmap where the key is a char in s and t 
-        # and the value is the number of occurences in s, t respectively 
-
-        hashS, hashT = {}, {} # initializes 2 hashmaps 
+        # keep count of each type of char in s
+        # then check if every char in t appears the same amount of 
+        # times as in s 
+        # store it in a dictionary 
+        dict_a = {}
+        dict_b = {}
         for x in range(len(s)):
-            hashS[s[x]] = 1 + hashS.get(s[x], 0)
-            hashT[t[x]] = 1 + hashT.get(t[x], 0)
-        for c in hashS:
-            # looping through hashS only, since hashS and hashT must be of the 
-            # same length till now 
-            if hashS[c] != hashT.get(c, 0):
+            if s[x] in dict_a:
+                dict_a[s[x]] += 1
+            else:
+                dict_a[s[x]] = 1
+        for x in range(len(t)):
+            if t[x] in dict_b:
+                dict_b[t[x]] += 1
+            else:
+                dict_b[t[x]] = 1
+        for x in range(len(t)):
+            if t[x] in dict_a:
+                if(dict_b[t[x]] == dict_a[t[x]]):
+                    continue
+                else:
+                    return False
+            else:
                 return False
         return True
-
-
-        
-        
