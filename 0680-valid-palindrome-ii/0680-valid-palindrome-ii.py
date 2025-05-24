65d919279b8d5d36760e4342ff8dfa28852b9808
Aravsharma1 <73394747+Aravsharma1@users.noreply.github.com>
2025-05-24 12:56:09 -0400

    Time: 115 ms (7.79%), Space: 17.9 MB (76.21%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -2,34 +2,43 @@ class Solution:
     def validPalindrome(self, s: str) -> bool:
         left = 0
         right = len(s) - 1 
-        constraint = 1  # at most one deletion allowed
-
-        while left <= right:
-            if s[left] == s[right]:
+        constraint = 2
+        valid_palindrome = False
+        while(left <= right):
+            if(s[left] == s[right]):
+                valid_palindrome = True
                 left += 1
                 right -= 1
             else:
-                if constraint == 0:
+                constraint -= 1
+                if(constraint == 0):
                     return False
-                # Try skipping either the left or the right character
-                # Check if the rest of the string is a palindrome after skipping one
-                skip_left = True
-                i, j = left + 1, right
-                while i < j:
-                    if s[i] != s[j]:
-                        skip_left = False
-                        break
-                    i += 1
-                    j -= 1
-
-                skip_right = True
-                i, j = left, right - 1
-                while i < j:
-                    if s[i] != s[j]:
-                        skip_right = False
-                        break
-                    i += 1
-                    j -= 1
-
-                return skip_left or skip_right
-        return True
+                else:
+                    # move left or right pointer
+                    # if(s[right] == s[left + 1]):
+                    #     left += 1
+                    # else:
+                    #     right -= 1
+                    # check whether substring until right - 1 is a palindrome
+                    # check whether substring from left + 1 until end is a 
+                    # palindrome
+                    start_right = right - 1 # check this
+                    check_left = True
+                    temp_left = left
+                    while(temp_left <= start_right):
+                        if (s[temp_left] != s[start_right]):
+                            check_left = False
+                        temp_left += 1
+                        start_right -= 1
+                    start_left = left + 1 
+                    temp_right = right
+                    check_right = True
+                    while(start_left <= temp_right):
+                        if (s[start_left] != s[temp_right]):
+                            check_right = False
+                        start_left += 1
+                        temp_right -= 1
+                    return check_right or check_left
+        return valid_palindrome
+                
+                
