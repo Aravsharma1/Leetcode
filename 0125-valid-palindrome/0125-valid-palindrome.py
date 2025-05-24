1d8d0861784746837243a2916d4a8756cb65cd15
Aravsharma1 <73394747+Aravsharma1@users.noreply.github.com>
2025-05-23 23:18:24 -0400

    Time: 15 ms (13.54%), Space: 17.9 MB (83.43%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,17 +1,31 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        length = len(s)
-        d = s.lower()
-        start = 0
-        end = length - 1
-        while(start < end):
-            if not(d[start].isalnum()):
-                start += 1
-            elif not(d[end].isalnum()):
-                end -= 1
-            elif d[start] != d[end]:
-                return False
-            else:
-                start += 1
-                end -= 1
-        return True    
+        left = 0
+        right = len(s) - 1
+        is_palindrome = True
+        while(left <= right):
+            # check if elements are of alpanumeric type 
+            # then if of alphanumeric type checks if they are alphabet and if yes convert that to lowercase. 
+            # then compare regardless and update bool variable 
+            if((s[left].isalnum()) and (s[right].isalnum())):
+                if(s[left].isalpha() and s[right].isalpha()):
+                    temp_left = s[left].lower()
+                    temp_right = s[right].lower()
+                    if temp_left == temp_right:
+                        left += 1
+                        right -= 1
+                        is_palindrome = True
+                    else:
+                        return False
+                else:
+                    if (s[left] == s[right]):
+                        left += 1
+                        right -= 1
+                        is_palindrome = True
+                    else:
+                        return False
+            elif (not (s[left].isalnum())):
+                left += 1
+            elif (not (s[right].isalnum())):
+                right -= 1
+        return is_palindrome
