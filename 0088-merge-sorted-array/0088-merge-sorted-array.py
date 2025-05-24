52a6b2d5c9732eafdc29d0e6f7eb2c7b5778d197
Aravsharma1 <73394747+Aravsharma1@users.noreply.github.com>
2025-05-24 19:49:02 -0400

    Time: 0 ms (100%), Space: 17.7 MB (93.39%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,7 +1,9 @@
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         counter = 0
+        # Step 1: fill nums1 up with nums2
         for x in range(m, len(nums1)):
             nums1[x] = nums2[counter]
             counter += 1
+        # sort filled up nums1 using Python's sort() function. 
         nums1.sort()
