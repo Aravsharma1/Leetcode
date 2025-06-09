4859027f21d82dad4caace19d49926785aca9558
Aravsharma1 <73394747+Aravsharma1@users.noreply.github.com>
2025-06-08 20:31:08 -0400

    Time: 78 ms (82.62%), Space: 28.4 MB (90.32%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,36 +1,25 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        # left = 0
-        # right = len(height) - 1
-        # maxarea = 1
-        # while (left < right):
-        #     current_area = (right - left) * min(height[left], height[right])
-        #     if current_area > maxarea:
-        #         maxarea = current_area
-        #         left += 1
-        #         right -= 1
-        #     else:
-        #         left += 1
-        #         right -= 1
-        # return maxarea
-
-        #brute force O(n^2) solution: 
-        # maxarea = 0
-        # for x in range(len(height)):
-        #     for y in range(len(height)):
-        #         current_area = abs(y - x) * min(height[x], height[y])
-        #         if current_area > maxarea:
-        #             maxarea = current_area    
-        # return maxarea
-        # reattempting optimal solution: 
-        maxarea = 0
+        # Brute Force Solution:
+        '''
+        max_area = 0 
+        for x in range(len(height)):
+            for y in range(len(height)):
+                if (x != y):
+                    current_area = abs(x - y) * min(height[x], height[y])
+                    if current_area > max_area:
+                        max_area = current_area
+        return max_area
+        '''
+        max_area = 0
         left = 0
         right = len(height) - 1
-        while (left < right):
-            current_area = min(height[left], height[right]) * abs(left - right)
-            maxarea = max(current_area, maxarea)
-            if height[left] > height[right]:
-                right -= 1
-            elif height[left] <= height[right]:
+        while(left < right):
+            current_area = abs(left - right) * min(height[left], height[right])
+            if current_area > max_area:
+                max_area = current_area
+            if height[left] < height[right]:
                 left += 1
-        return maxarea
+            else:
+                right -= 1
+        return max_area
