5be126c63d6f1a3366c9f51b8f40533e873a6b01
Aravsharma1 <73394747+Aravsharma1@users.noreply.github.com>
2025-06-14 16:09:54 -0400

    Time: 59 ms (5.08%), Space: 21.1 MB (89.61%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -8,9 +8,26 @@ class Solution:
                 countElements[x] += 1
             else:
                 countElements[x] = 1
+        values_array = (len(countElements)) * [0]
+        #key technique: can keep track of length of dictionary using len function (like for python)
+        counter = 0
+        for x in countElements:
+            values_array[counter] = countElements[x]
+            counter += 1
+        values_array.sort()
+        final_array = [0] * k
+        copy_k = k 
+        counter = 0
+        used_keys = set()
+        while(copy_k > 0):
+            for key,value in countElements.items():
+                if key in used_keys:
+                    continue
+                if values_array[-copy_k] == value:
+                    final_array[counter] = key
+                    counter += 1
+                    copy_k -= 1
+                    used_keys.add(key)
+                    break  
+        return final_array
 
-        # Step 2: Sorting the keys based on their frequencies in descending order
-        sorted_keys = sorted(countElements, key=lambda x: countElements[x],reverse=True)
-
-        # Step 3: Return the first k elements in the sorted keys
-        return list(sorted_keys[:k])
